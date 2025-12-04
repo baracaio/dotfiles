e6bf353262e523cf8df27da0b7a167c086d0e90e
cbaracat_meli <caio.baracat@mercadolivre.com>
2025-12-04 14:27:13 -0300

    add db host exports

--- zsh/meli.zsh
+++ zsh/meli.zsh
@@ -2,3 +2,8 @@ alias meli="cd ~/dev/meli"
 
 ## applications
 alias xo="cd ~/dev/meli/fury_exchange-office"
+
+export DB_HOST_REMOTE_REPLICA="proxysql.slave.ohio.meliseginf.com"
+export DB_HOST_LOCAL_REPLICA="proxysql.slave.meliseginf.com"
+export DB_HOST_MASTER="proxysql.master.meliseginf.com"
+export DB_HOST_CLONE="proxysql.clone.meliseginf.com"
